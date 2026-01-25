1b4f708c3bf33c58e7030abe125c501fcf69bd95
kimtai1-77 <167165422+kimtai1-77@users.noreply.github.com>
2026-01-25 10:29:39 +0300

    Separate logic from html file

---
+++ scripts/player-enter-name.js
@@ -0,0 +1,23 @@
+
+
+    const proceedButton = document.querySelector('[data-action="proceed"]');
+    proceedButton.addEventListener("click", () => {
+        captureName();
+    })
+    
+    function captureName () {
+        const singlePlayerName = document.getElementById("singlePlayerName").value;
+
+        // check if name is empty
+        if (!singlePlayerName) {
+            alert("Please enter a name before proceeding")
+            return;
+        }
+
+        //build query parameters
+        const params = new URLSearchParams();
+        params.set("single-player-name", singlePlayerName);
+        params.set("mode", "single");
+
+        window.location.href = "./start.html?" + params.toString();
+    }
